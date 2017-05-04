2673695d892b217bf043814f833cb49b348f40cd
Arthur Gautier <baloo@gandi.net>
2017-05-04 19:30:13 +0000

    gsqlite3: Ensure module is not loaded when invoked directly
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- modules/gsqlite3backend/gsqlite3backend.cc
+++ modules/gsqlite3backend/gsqlite3backend.cc
@@ -156,22 +156,3 @@ DNSBackend * gSQLite3Factory::make( const string & suffix)
 }
 
 
-//! Magic class that is activated when the dynamic library is loaded
-class gSQLite3Loader
-{
-public:
-  //! This reports us to the main UeberBackend class
-  gSQLite3Loader()
-  {
-    BackendMakers().report( new gSQLite3Factory( "gsqlite3" ));
-    L << Logger::Info << "[gsqlite3] This is the gsqlite3 backend version " VERSION
-#ifndef REPRODUCIBLE
-      << " (" __DATE__ " " __TIME__ ")"
-#endif
-      << " reporting" << endl;
-  }
-};
-
-//! Reports the backendloader to the UeberBackend.
-static gSQLite3Loader gsqlite3loader;
-
