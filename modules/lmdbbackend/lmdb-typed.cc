f2751339684baa5daf77d6bf31f56344c742e832
Pieter Lexis <pieter.lexis@powerdns.com>
2019-02-12 08:28:00 +0100

    Move lmdb-{typed,safe}.{cc,hh} to /pdns for re-use

--- modules/lmdbbackend/lmdb-typed.cc
+++ modules/lmdbbackend/lmdb-typed.cc
@@ -1,14 +1,1 @@
-#include "lmdb-typed.hh"
-
-unsigned int MDBGetMaxID(MDBRWTransaction& txn, MDBDbi& dbi)
-{
-  auto cursor = txn.getCursor(dbi);
-  MDBOutVal maxidval, maxcontent;
-  unsigned int maxid{0};
-  if(!cursor.get(maxidval, maxcontent, MDB_LAST)) {
-    maxid = maxidval.get<unsigned int>();
-  }
-  return maxid;
-}
-
-
+../../pdns/lmdb-typed.cc
